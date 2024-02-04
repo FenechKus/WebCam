06afd92ad355c516da994ca2ce759162a2971ebc
Kus Vlz <max1.mmm@yandex.ru>
2024-02-04 14:52:19 +0300

    Add Streaming video from WebCam

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System.IO;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 using System.Drawing;
 using System.Drawing.Imaging;
+using AForge.Video;
 using AForge.Video.DirectShow;
-using Image = System.Drawing.Image;
 
 namespace WebCam;
 
@@ -22,8 +13,70 @@ namespace WebCam;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private VideoCaptureDevice _videoSource;
     public MainWindow()
     {
         InitializeComponent();
+        InitializeWebCam();
+    }
+
+    private void InitializeWebCam()
+    {
+        FilterInfoCollection videoDevice = new(FilterCategory.VideoInputDevice);
+        if (videoDevice.Count > 0)
+        {
+            _videoSource = new VideoCaptureDevice(videoDevice[0].MonikerString);
+            _videoSource.NewFrame += VideoSource_NewFrame;
+        }
+        else
+            MessageBox.Show("Video devices not found", "Info", MessageBoxButton.OK, MessageBoxImage.Warning);
+    }
+
+    private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+    {
+        try
+        {
+            BitmapImage bi = ConvertBitmapToBitmapImage(eventArgs.Frame);
+            bi.Freeze();
+
+            Dispatcher.Invoke(() =>
+            {
+                ImageBox.Source = bi;
+            });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+    {
+        using (MemoryStream memoryStream = new())
+        {
+            bitmap.Save(memoryStream, ImageFormat.Bmp);
+            memoryStream.Position = 0;
+
+            BitmapImage bitmapImage = new();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memoryStream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            return bitmapImage;
+        }
+    }
+
+    private void StartButton(object sender, RoutedEventArgs e) => _videoSource.Start();
+
+    private void StopButton(object sender, RoutedEventArgs e)
+    {
+        base.OnClosed(e);
+        if (_videoSource.IsRunning)
+        {
+            _videoSource.SignalToStop();
+            _videoSource.WaitForStop();
+            ImageBox.Source = null;
+        }
     }
 }
